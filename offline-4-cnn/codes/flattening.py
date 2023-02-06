dff36e972cc63546c635815f909da16a6a28326b
asifajrof <1705092@ugrad.cse.buet.ac.bd>
2023-02-06 18:04:30 +0600

    flattening order transpose

--- offline-4-cnn/codes/flattening.py
+++ offline-4-cnn/codes/flattening.py
@@ -9,13 +9,13 @@ class Flattening(Layer):
 
     def forward(self, input):
         # input: convolutional filter map
-        # output: flattened input. as column vectors.
+        # output: flattened input. as row vectors.
         self.input_shape = input.shape
-        output = np.transpose(input.reshape(input.shape[0], -1))
+        output = input.reshape(input.shape[0], -1)
         return output
 
     def backward(self, output_error, learning_rate):
         # output_error: error of the next layer
         # learning_rate: learning rate
-        input_error = np.transpose(output_error).reshape(self.input_shape)
+        input_error = output_error.reshape(self.input_shape)
         return input_error
