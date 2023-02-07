0925824356959c2126245dca869a04db250aa0ff
asifajrof <1705092@ugrad.cse.buet.ac.bd>
2023-02-08 03:08:46 +0600

    maxpool backprop done

--- offline-4-cnn/codes/maxpooling.py
+++ offline-4-cnn/codes/maxpooling.py
@@ -1,4 +1,3 @@
-# max pooling layer, inherits from layer class
 from layer import Layer
 import numpy as np
 
@@ -19,6 +18,8 @@ class MaxPooling(Layer):
             (height - self.filter_height) / self.stride + 1)
         output_width = int(
             (width - self.filter_width) / self.stride + 1)
+
+        # # for loop
         # output = np.zeros(
         #     (batch_size, num_channels, output_height, output_width))
         # for h in range(output_height):
@@ -52,17 +53,101 @@ class MaxPooling(Layer):
             )
         )
         output = np.max(input_strided, axis=(4, 5))
+
+        # special case when stride == filter_size
+        self.max_value_mask = None
+        # max_value_mask -> (batch_size, num_channels, input_height, input_width)
+        if self.stride == self.filter_height:
+            # initialize from output. repeating
+            self.max_value_mask = output.repeat(
+                self.stride,
+                axis=2
+            ).repeat(
+                self.stride,
+                axis=3
+            )
+            # pad for non-divisible input size
+            max_pad_height = height - self.max_value_mask.shape[2]
+            max_pad_width = width - self.max_value_mask.shape[3]
+            if max_pad_height > 0 or max_pad_width > 0:
+                self.max_value_mask = np.pad(
+                    self.max_value_mask,
+                    (
+                        (0, 0),
+                        (0, 0),
+                        (0, max_pad_height),
+                        (0, max_pad_width)
+                    ),
+                    'constant',
+                )
+            # compare with input
+            # problem for multiple maxima :(
+            self.max_value_mask = np.equal(self.max_value_mask, input)
+
         return output
 
     def backward(self, output_error, learning_rate):
         # output_error. (batch_size, num_channels, out_height, out_width)
-        input_error = np.zeros(self.input.shape)
         batch_size, num_channels, height, width = self.input.shape
-        # output_height, output_width = output_error.shape[1:]
-        # for channel in range(num_channels):
-        #     for h in range(output_height):
-        #         for w in range(output_width):
-        #             input_error[channel, h * self.stride:h * self.stride + self.filter_size, w * self.stride:w * self.stride + self.filter_size] += np.where(self.input[channel, h * self.stride:h * self.stride + self.filter_size, w * self.stride:w * self.stride + self.filter_size] == np.max(
-        #                 self.input[channel, h * self.stride:h * self.stride + self.filter_size, w * self.stride:w * self.stride + self.filter_size]), output_error[channel, h, w], 0)
-        # return input_error
-        pass
+        # special case when stride == filter_size
+        if self.stride == self.filter_height:
+            # check if max_value_mask is not None
+            if self.max_value_mask is None:
+                raise Exception(
+                    'max_value_mask is None. Check if stride == filter_height')
+            else:
+                # output error needs to be tiled
+                repeated_output_error = output_error.repeat(
+                    self.stride,
+                    axis=2
+                ).repeat(
+                    self.stride,
+                    axis=3
+                )
+                # pad for non-divisible input size
+                pad_height = height - repeated_output_error.shape[2]
+                pad_width = width - repeated_output_error.shape[3]
+                if pad_height > 0 or pad_width > 0:
+                    repeated_output_error = np.pad(
+                        repeated_output_error,
+                        (
+                            (0, 0),
+                            (0, 0),
+                            (0, pad_height),
+                            (0, pad_width)
+                        ),
+                        'constant',
+                    )
+                # element-wise multiplication
+                input_error = np.einsum(
+                    'ijkl,ijkl->ijkl',
+                    self.max_value_mask,
+                    repeated_output_error
+                )
+
+        else:
+            # for loop
+            _, _, output_height, output_width = output_error.shape
+            input_error = np.zeros(self.input.shape)
+
+            for i in range(batch_size):
+                for j in range(num_channels):
+                    for h in range(output_height):
+                        for w in range(output_width):
+                            input_window = self.input[
+                                i,
+                                j,
+                                h*self.stride:h*self.stride+self.filter_height,
+                                w*self.stride:w*self.stride+self.filter_width
+                            ]
+                            # https://stackoverflow.com/a/9483964
+                            max_index = np.unravel_index(
+                                input_window.argmax(), input_window.shape)
+                            max_index = (i, j, h*self.stride +
+                                         max_index[0], w*self.stride+max_index[1])
+
+                            # add overlapped gradients
+                            # https://ai.stackexchange.com/a/17109
+                            input_error[max_index] += output_error[i, j, h, w]
+
+        return input_error
